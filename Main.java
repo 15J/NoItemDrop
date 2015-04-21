40acbfe553a71843fc2ead3b3f3ea7361b655019
15J <jeroen15j@hotmail.com>
2015-04-21 10:26:03 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+package me.gewoonjeroenn.noitemdrop;
+
+import java.util.Arrays;
+
+import me.gewoonjeroenn.noitemdrop.listeners.DropItemEvent;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener{
+	
+	public void onEnable(){
+		PluginManager pm = Bukkit.getServer().getPluginManager();
+		pm.registerEvents(new DropItemEvent(), this);
+	     
+		loadConfiguration();
+	}
+	
+	public void loadConfiguration(){
+	     getConfig().options().copyDefaults(true); 
+	     String[] list = {"world", "world", "world", "world"};
+	     getConfig().set("worlds", Arrays.asList(list));
+	     saveConfig();
+
+}}
